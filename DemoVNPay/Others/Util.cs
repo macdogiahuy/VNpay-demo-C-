917e856acc0ea40dcbc7d82be3fcfe80a4a95f17
Mạc Đỗ Gia Huy <macdogiahuy123@gmail.com>
2023-09-23 15:39:13 +0700

    Initial commit

---
+++ DemoVNPay/Others/Util.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace DemoVNPay.Others
+{
+    public class Util
+    {
+        public static String HmacSHA512(string key, String inputData)
+        {
+            var hash = new StringBuilder();
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+            using (var hmac = new HMACSHA512(keyBytes))
+            {
+                byte[] hashValue = hmac.ComputeHash(inputBytes);
+                foreach (var theByte in hashValue)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+
+            return hash.ToString();
+        }
+
+        public static string GetIpAddress()
+        {
+            string ipAddress;
+            try
+            {
+                ipAddress = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+                if (string.IsNullOrEmpty(ipAddress) || (ipAddress.ToLower() == "unknown"))
+                    ipAddress = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+            catch (Exception ex)
+            {
+                ipAddress = "Invalid IP:" + ex.Message;
+            }
+
+            return ipAddress;
+        }
+    }
+}
